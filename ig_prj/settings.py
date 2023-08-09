f8eabafb4f3c58fba5a71a05ec655522d7732b32
vedpatel7 <vedvirpara2004@gmail.com>
2023-08-09 19:08:35 +0530

    modified:   ig_prj/settings.py

--- ig_prj/settings.py
+++ ig_prj/settings.py
@@ -98,7 +98,11 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = [
+    os.path.join(BASE_DIR, 'staticfiles'),
+    os.path.join(BASE_DIR, 'staticfiles_build','static')
+
+]
 
 
 MEDIA_URL = '/media/'
