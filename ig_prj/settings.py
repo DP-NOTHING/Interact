15e930d3738b86cf80391501b20fac8b8b1feb2a
DP-NOTHING <deep1907rp@gmail.com>
2023-08-09 17:35:16 +0530

    modified:   build_files.sh 	modified:   ig_prj/settings.py 	modified:   vercel.json

--- ig_prj/settings.py
+++ ig_prj/settings.py
@@ -98,7 +98,9 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = [os.path.join(BASE_DIR, 'staticfiles'),os.path.join(BASE_DIR, 'staticfiles_build','static')]
+
+
 
 MEDIA_URL = '/media/'
 
