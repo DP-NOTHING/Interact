65ebf7d78681e204240a6e743757c7cc0bd36e88
DP-NOTHING <deep1907rp@gmail.com>
2023-08-09 17:14:06 +0530

    modified:   build_files.sh 	modified:   vercel.json

--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,4 @@
 echo " BUILD START "
-python3.11 -m pip install -r requirnment.txt
-python3.11 manage.py collectstatic --noinput --clear
+py -m pip install -r requirnments.txt
+py manage.py collectstatic --noinput --clear
 echo " BUILD END"
